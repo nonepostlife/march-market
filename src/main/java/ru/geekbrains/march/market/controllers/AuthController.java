846a93ead2138fa7b6f1491fc4e2849cf99f299a
Ilya <ilyha575@inbox.ru>
2022-04-09 13:58:46 +1000

    add hw3 security

---
+++ src/main/java/ru/geekbrains/march/market/controllers/AuthController.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.march.market.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.geekbrains.march.market.dtos.JwtRequest;
+import ru.geekbrains.march.market.dtos.JwtResponse;
+import ru.geekbrains.march.market.exceptions.AppError;
+import ru.geekbrains.march.market.services.UserService;
+import ru.geekbrains.march.market.utils.JwtTokenUtil;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(new AppError("CHECK_TOKEN_ERROR", "Некорректный логин или пароль"), HttpStatus.UNAUTHORIZED);
+        }
+        UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
+        String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+}
---
+++ src/main/java/ru/geekbrains/march/market/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.march.market.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.march.market.entities.Role;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
